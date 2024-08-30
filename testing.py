ea91182accdfc0924a3b6d97bc30d930eb81b475
lcri728 <lcri728@aucklanduni.ac.nz>
2024-08-30 23:19:33 +1200

    Setup pycharm project

--- testing.py
+++ testing.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
-pitch_width = 120  
-pitch_length = 80  
+pitch_width = 120
+pitch_length = 80
+
 
 def onclick(event):
     # Get the x and y coordinates where the user clicked
@@ -11,14 +12,16 @@ def onclick(event):
     if x is not None and y is not None:
         print(f"{y:.2f}, {x:.2f}")
 
+
 img = mpimg.imread('football_pitch_image.png')
 
+
 fig, ax = plt.subplots()
-ax.imshow(img, extent=[0,pitch_length,59, (pitch_width)])  # Display the image as the background
+ax.imshow(img, extent=[0, pitch_length, 59, (pitch_width)])  # Display the image as the background
 
 # Set plot limits and labels
-ax.set_xlim(0,pitch_length)
-ax.set_ylim(59,pitch_width)
+ax.set_xlim(0, pitch_length)
+ax.set_ylim(59, pitch_width)
 ax.set_aspect('equal', adjustable='box')
 ax.set_xlabel('Width (meters)')
 ax.set_ylabel('Length (meters)')
